4a3afc75b5043c1c87c9909b974e0db84d26b0d0
slumpsand <slumpsand@gmail.com>
2017-07-22 19:38:09 +0200

    IT WORKS!

--- asm/boot.asm
+++ asm/boot.asm
@@ -4,24 +4,19 @@ KERNEL_OFFSET equ 1000h
 
     mov [BOOT_DRIVE], dl                  ; dl will point at the boot drive, save it
 
-    mov bp, 9000h                        ; move the stack somewhere else
+    mov bp, 9000h                         ; move the stack somewhere else
     mov sp, bp
 
-    mov bx, KERNEL_OFFSET                 ; load the kernel into memory
-    mov dh, 2
-    mov dl, [BOOT_DRIVE]
+    mov bx, KERNEL_OFFSET
+    mov dh, 1                             ; for some reason this was '2' in the tutorial
+    mov dl, [BOOT_DRIVE]                  ; but that didn't work ...
 
     call disk_load
-
     jmp switch_pm                         ; will never return
 
 %include "print.asm"
 %include "enter.asm"
 %include "disk.asm"
-
-BOOT_DRIVE db 0
-Step1 db "Step 1!!!", 0
-Step2 db "Step 2!!!", 0
 
 [bits 32]                                 ; will be executed in 32bit,
 ENTER:                                    ; protected mode
@@ -29,6 +24,8 @@ ENTER:                                    ; protected mode
     call KERNEL_OFFSET                    ; start the kernel   
  
     jmp $
+
+BOOT_DRIVE db 0
 
 times 510 - ($-$$) db 0                   ; fill the boot sector 
 dw 0xAA55
