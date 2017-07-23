5dacfec1c8c7d5d529ac87e16fbab23946f78a2e
slumpsand <slumpsand@gmail.com>
2017-07-23 17:18:01 +0200

    add gdb to the mix ...

--- cross.mk
+++ cross.mk
@@ -3,22 +3,26 @@ SHELL := /bin/bash
 A := $(shell tput setaf 3 && tput bold)
 B := $(shell tput sgr0)
 
+export PATH := "/opt/cross/bin:$(PATH)"
+
 build:
 	@read -p "$(A)building at: '$(PWD)', ok? $(B)" -n 1 -r && test "$$REPLY" = "y"
 	@echo
 
-	mkdir -p build-gcc build-binutils
+	mkdir -p build-gcc build-binutils build-gdb
 
 	@echo "$(A)(01) downloading ...$(B)"
 	curl -O https://ftp.gnu.org/gnu/gcc/gcc-7.1.0/gcc-7.1.0.tar.gz
 	curl -O https://ftp.gnu.org/gnu/binutils/binutils-2.28.tar.gz
+	curl -O https://ftp.gnu.org/gnu/gdb/gdb-8.0.tar.gz
 
 	@echo "$(A)(02) extracting ...$(B)"
 	tar -xf gcc-7.1.0.tar.gz
 	tar -xf binutils-2.28.tar.gz
+	tar -xf gdb-8.0.tar.gz
 
 	@echo "$(A)(03) configuring binutils ...$(B)"
-	cd build-binutils && PATH="/opt/cross/bin:$$PATH" ../binutils-2.28/configure --prefix=/opt/cross \
+	cd build-binutils && ../binutils-2.28/configure --prefix=/opt/cross \
 	     --target=i386-elf --enable-interwork --enable-multilib --disable-nls \
 	     --disable-werror
 
@@ -30,7 +34,7 @@ build:
 
 	@echo "$(A)(06) configuring gcc ...$(B)"
 	cd gcc-7.1.0 && ./contrib/download_prerequisites
-	cd build-gcc && PATH="/opt/cross/bin:$$PATH" ../gcc-7.1.0/configure --prefix=/opt/cross \
+	cd build-gcc && ../gcc-7.1.0/configure --prefix=/opt/cross \
 	    --target=i386-elf --disable-nls --disable-libssp --enable-languages=c \
 	    --without-headers
 
@@ -45,3 +49,13 @@ build:
 
 	@echo "$(A)(10) installing libgcc ...$(B)"
 	cd build-gcc && make install-target-libgcc
+
+	@echo "$(A)(11) configuring gdb ...$(B)"
+	cd build-gdb && ../gdb-8.0/configure --prefix=/opt/cross --target=i386-elf \
+		--program-prefix=i386-elf-
+
+	@echo "$(A)(12) building gdb ...$(B)"
+	cd build-gdb && make -j8
+
+	@echo "$(A)(13) installing gdb ...$(B)"
+	cd build-gdb && make install
