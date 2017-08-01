a6652777db61b2e14be3f3eba2d2d138590bdd9a
slumpsand <slumpsand@gmail.com>
2017-08-01 21:35:42 +0200

    found another bug (unfixed)

--- kernel/main.c
+++ kernel/main.c
@@ -2,24 +2,5 @@
 
 void main() {
   vga_text_init();
-
-  println("01");
-  println("02");
-  println("03");
-  println("04");
-  println("05");
-  println("06");
-  println("07");
-  println("08");
-  println("09");
-  println("10");
-  println("11");
-  println("12");
-  println("13");
-  println("14");
-  println("15");
-  println("16");
-  println("17");
-  println("18");
-  println("19");
+  vga_text_clear();
 }
