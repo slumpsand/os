e82d53513d62a731b203e06589a41c45d7768ee0
slumpsand <slumpsand@gmail.com>
2017-08-06 15:11:10 +0200

    yes, I was disassembling the wrong file ...

--- scripts/disasm.sh
+++ scripts/disasm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ndisasm -u out/all.elf | sed -re "s/^[0-9A-F]+\s+[0-9A-F]+\s+(.*)$/\1/g" > out/disasm.asm
+ndisasm -u out/all.bin | sed -re "s/^[0-9A-F]+\s+[0-9A-F]+\s+(.*)$/\1/g" > out/disasm.asm
 code out/disasm.asm
